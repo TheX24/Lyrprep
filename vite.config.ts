dc51bb2e6365f0130a89d33a6f6b91dbfe3dee01
SpikeNew7774 <newadm159@gmail.com>
2025-08-30 17:34:09 +0200

    Cache guide/

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,11 @@ export default defineConfig({
               },
             },
           },
+          {
+            urlPattern: /^\/guide\/?$/,
+            handler: "NetworkFirst",
+            options: { cacheName: "html-guide" }
+          }
         ],
       },
     }),
