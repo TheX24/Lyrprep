7b28318780c0bb8f876ae908c50d67a876131e69
SpikeNew7774 <newadm159@gmail.com>
2025-08-30 17:21:45 +0200

    Fix Cache bugs, Replace image urls, Fixed some stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: 'auto',
       devOptions: { enabled: false },
       manifest: {
         name: "Lyrprep",
@@ -26,7 +27,8 @@ export default defineConfig({
         screenshots: [],
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,woff,woff2,ttf,eot}"],
+        navigateFallbackDenylist: [/^\/guide(\/.*)?$/],
+        globPatterns: ["**/*.{js,css,html,png,gif,svg,woff,woff2,ttf,eot}"],
         runtimeCaching: [
           // External Image Caching
           {
@@ -56,4 +58,12 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        guide: "guide/index.html",
+      },
+    },
+  },
 });
