f4186748c8c9a3c5ddb9a3c37a1b249169d82b14
SpikeNew7774 <newadm159@gmail.com>
2025-09-17 14:52:38 +0200

    Add font options

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,11 @@
 declare global {
 	interface Window {
 		onloadHCaptcha: () => void;
+		InternalEvent: {
+			listen: (event: string, handler: (data: any) => void) => number;
+			unListen: (event: string, id: number) => void;
+			evoke: (event: string, data: any) => void;
+		};
 	}
 	// Minimal ambient typing for hCaptcha injected by external script
 	var hcaptcha: any;
